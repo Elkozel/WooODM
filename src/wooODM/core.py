ddf7a0c30c842f5a1cd79b75bdfc0e3c66af5cfd
Tsvetomir Hristov <caci802@gmail.com>
2025-03-07 18:19:30 +0100

    Improving the WooCommerce class

--- src/wooODM/core.py
+++ src/wooODM/core.py
@@ -1,17 +1,29 @@
-import requests
 from woocommerce import API  # Install using `pip install woocommerce`
 
 class WooCommerce:
+    """
+    A singleton class to interact with the WooCommerce API.
+    """
     _instance = None
 
-    def __init__(self, url, consumer_key, consumer_secret):
-        wcapi = API(
+    def __init__(self):
+        pass
+
+    @classmethod
+    def init(cls, url, consumer_key, consumer_secret):
+        """
+        Initializes the WooCommerce API instance.
+        Args:
+            url (str): The base URL for the WooCommerce store.
+            consumer_key (str): The consumer key for the WooCommerce API.
+            consumer_secret (str): The consumer secret for the WooCommerce API.
+        """
+        cls._instance = API(
             url=url,
             consumer_key=consumer_key,
             consumer_secret=consumer_secret,
             version="wc/v3"
         )
-        WooCommerce._instance = wcapi
 
     @classmethod
     def get_instance(cls):
